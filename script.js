0ff3ee9e91b7371b4130f4614654bc5a9075d227
Navaneeth <navaneethkulal8@gmail.com>
2022-07-26 17:13:28 +0530

    just an edit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const form =document.getElementById('form')
 const input =document>getElementById('input')
-
+const todos =document>getElementById('todos')
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
 
 
     const todo =  input.value;
     if (todo){
-        
+        const todoEl = document.createElement
+        ("li");
+        todoEl.innnerText = todo;
+        todos.appendChild(todo);
     }
 
 
