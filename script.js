7bc479df3adf9ab111c3581fbea9e673ce53086d
Navaneeth <navaneethkulal8@gmail.com>
2022-07-27 12:41:02 +0530

    tutorial

--- script.js
+++ script.js
@@ -1,17 +1,69 @@
-const form =document.getElementById('form')
-const input =document>getElementById('input')
-const todos =document>getElementById('todos')
-form.addEventListener('submit',(e)=>{
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 
+    addTodo();
+});
 
-    const todo =  input.value;
-    if (todo){
-        const todoEl = document.createElement
-        ("li");
-        todoEl.innnerText = todoText;
-        todos.appendChild(todoEl);
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.text;
     }
 
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
 
-});
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+
+            updateLS();
+        });
+
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        });
+
+        todosUL.appendChild(todoEl);
+
+        input.value = "";
+
+        updateLS();
+    }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll("li");
+
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
